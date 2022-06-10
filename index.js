a3017076c5c7aa28e6f16d45d7aaf80ffc231825
tmoino <tony.moino@student.moringaschool.com>
2022-06-10 14:58:15 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards( namesArray, event ) {
+  let messagesCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    messagesCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return messagesCards
+
+}
+
+function countDown (num){
+  while ( num > 0){
+    console.log(num);
+    num -=1
+
+  }
+  console.log (num);
+}
